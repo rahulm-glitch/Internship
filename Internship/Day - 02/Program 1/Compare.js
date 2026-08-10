f6a3d754d7105d4aa78d7a4d4fa50f2d5afe0bc0
Rahul Mali <rahulm8014@gmail.com>
2026-08-10 21:14:45 +0530

    Add files via upload

---
+++ Internship/Day - 02/Program 1/Compare.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 15;
+
+if (a > b) {
+    console.log("a is bigger");
+} else if (b > a) {
+    console.log("b is bigger");
+} else {
+    console.log("Both values are equal");
+}
