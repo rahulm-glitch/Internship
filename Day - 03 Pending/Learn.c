112173e7c3bea8cd2d8e1363745d27d8a4ef1a1d
Rahul Mali <rahulm8014@gmail.com>
2026-08-12 21:10:38 +0530

    Push Try

---
+++ Day - 03 Pending/Learn.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // This line prints the message to the screen
+    printf("Hello, World!\n");
+    
+    // This tells the system that the program finished successfully
+    return 0;
+}
