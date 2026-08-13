21de40baa76dba8428cff308674ffc20b86f1d31
Rahul Mali <rahulm8014@gmail.com>
2026-08-13 18:29:54 +0530

    Day - 05

---
+++ Day - 05/Program2/ShiftLeft.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 9,b = 7,c;
+    c = a<<b;
+    printf("The value of c is %d",c);
+}
---
+++ Day - 05/Program3/ShiftRight.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a = 20 , b = 2 , c;
+    c = a >> b;
+    printf("Result %d",c);
+}
